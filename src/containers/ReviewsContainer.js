2944e0c9b099bd15636133e221f7d5cf2fb09ebf
dean <deanlloyd64@gmail.com>
2020-02-28 14:52:23 -0500

    Done.

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 
+import { connect } from 'react-redux'
+import Restaurant from '../components/restaurants/Restaurant'
+
 class ReviewsContainer extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurant={this.props.restaurant}/>
+        <Reviews reviews={this.props.reviews} deleteReview={this.props.deleteReview} restaurant={this.props.restaurant} restaurantId={this.props.restaurant.id}/>
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => {
+  return{
+    reviews: state.reviews.reviews
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addReview: payload => dispatch({type: 'ADD_REVIEW', payload}),
+  deleteReview: id => dispatch({type: 'DELETE_REVIEW', id})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import manageRestaurant from './reducers/manageRestaurant'
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { allReducers } from './reducers/index'
 
-const store = createStore(manageRestaurant)
+const store = createStore(allReducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
 ReactDOM.render(
